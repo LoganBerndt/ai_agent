22c2781e389de948ea1ab1582b7b46eb5b644ede
loganberndt <loganberndt12@gmail.com>
2026-07-08 14:02:15 -0500

    Coded in the entirety of CH1: LLMS

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from openai import OpenAI
+
+def main():
+
+    load_dotenv()
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+
+    if api_key is None:
+        raise RuntimeError("API Key not found.")
+
+    client = OpenAI(
+        base_url="https://openrouter.ai/api/v1",
+        api_key=api_key,
+    )
+
+
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    messages = [
+            {
+                "role": "user",
+                "content": args.user_prompt,
+            }
+        ]
+
+    response = client.chat.completions.create(
+        model="openrouter/free",
+        messages=messages
+    )
+
+    check_usage = response.usage
+
+    
+    if args.verbose == True:
+        prompt_tokens = response.usage.prompt_tokens
+        completion_tokens = response.usage.completion_tokens
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {completion_tokens}")
+        print(f"User prompt: {args.user_prompt}")
+        
+    print(response.choices[0].message.content)
+
+
+
+
+main()
